069f16e859903500bd7dd9e9e29cad8ef9978a89
Brahmaji212 <80663867+Brahmaji212@users.noreply.github.com>
2021-11-26 11:38:16 +0530

    Update Armstrong.php

--- Armstrong.php
+++ Armstrong.php
@@ -2,7 +2,7 @@
 echo "------------- Armstrong Number ---------";
 
 
-$count=500;
+$count=9999;
 for($n=0;$n<=$count;$n++){
 $num=$n;
 $rem=0;
@@ -10,12 +10,36 @@ $sum=0;
 while($num > 0)
 {
     $rem=$num%10;
+    if($num < 1000){
     $sum=$sum+$rem*$rem*$rem;
     $num=$num/10;
+    }
+    else if($num >= 1000){
+    $sum=$sum+$rem*$rem*$rem*$rem;
+    $num=$num/10;
+    }
+    
 }
 if($n == $sum)
 {
-    echo "\n $n is Armstrong number ";
+    echo "\n $n is Armstrong number  \n";
+}
+
+}
+
+for($n=10;$n<=200;$n++){
+$rem=0;
+$sum=0;
+$num=$n;
+while(floor($num) )
+{
+    $rem=$num%10;
+    $sum=$sum*10+$rem;
+    $num=$num/10;   
+}
+if($n == $sum)
+{
+    echo "\n $sum is palindrome \n";
 }
 
 }
